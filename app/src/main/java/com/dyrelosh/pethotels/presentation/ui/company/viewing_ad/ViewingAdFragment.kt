9eba9856f77e8d50207d537419b077d21f4d32f2
Александр Прокин <aaprokin90@yandex.ru>
2023-05-04 17:22:17 +0300

    Исправлено получение id объявления у отеля

--- app/src/main/java/com/dyrelosh/pethotels/presentation/ui/company/viewing_ad/ViewingAdFragment.kt
+++ app/src/main/java/com/dyrelosh/pethotels/presentation/ui/company/viewing_ad/ViewingAdFragment.kt
@@ -5,66 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResultListener
-import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
-import com.dyrelosh.pethotels.R
 import com.dyrelosh.pethotels.databinding.FragmentViewingAdBinding
- import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.dyrelosh.pethotels.presentation.ui.company.company_ads.CompanyAdsFragment
+import com.squareup.picasso.Picasso
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class ViewingAdFragment : Fragment() {
 
     lateinit var binding: FragmentViewingAdBinding
     private val viewModel by viewModel<CompanyViewingAdViewModel>()
-    private var param1: String? = null
+    private var itemId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-             param1 = arguments?.getString(ARG_PARAM1)
-
+        itemId = arguments?.getString(CompanyAdsFragment.PAIR_KEY)
     }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentViewingAdBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-        viewModel.getAddInfo(param1.toString())
+        itemId?.let { viewModel.getAddInfo(it) }
 
         viewModel.addInfo.observe(viewLifecycleOwner) { addInfo ->
-            binding.titleViewingAd.text = addInfo.name
-            binding.citiTextViewViewingAd.text = addInfo.city
-            binding.addressTextViewViewingAd.text = addInfo.address
-            binding.descriptionTextViewViewingAd.text = addInfo.description
-            binding.numberTextViewViewingAd.text = addInfo.number
+            with(binding){
+                titleViewingAd.text = addInfo.name
+                citiTextViewViewingAd.text = addInfo.city
+                addressTextViewViewingAd.text = addInfo.address
+                descriptionTextViewViewingAd.text = addInfo.description
+                numberTextViewViewingAd.text = addInfo.number
+                Picasso.get().load("")
+                        // TODO нужно добавить плейсхолдер, на то время пока загружается картинка
+                   // .placeholder(R.drawable.hotel_placeholder)
+                    .into(photoViewingAd)
+            }
         }
 
         binding.imageBack.setOnClickListener {
             findNavController().popBackStack()
         }
-
     }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(id: String) {
-            ViewingAdFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, id)
-                }
-            }
-        }
-        private const val ARG_PARAM1 = "ViewingAdFragment"
-
-    }
-
 }
